56b032a1fe0fcc49492414f44e073cd4276b653b
Jean Do <jean.dominique.lecomte.delisle@protonmail.me>
2023-12-31 09:16:52 -0400

    varnames more explicit for time units, updated perf notes

--- brainstorm/polling_bootsel_button.py
+++ brainstorm/polling_bootsel_button.py
@@ -8,8 +8,13 @@ brainstorm:
    pico is idle for all but 1/1000th of its cycles.  To me, this means even 
    polling could be very responsive to button presses and releases (10-40/s)
 
+
    Still, setting irqs on the rising and falling edges with a timer seems
    the better way (but not sure yet how to do it with the bootsel_button).
+   With more logic in the loop (like exists below) the pico is busy ~170us
+   instead of 25-30us noted above w/o useful logic, indicating that pico is
+   still idle 99.9932% during press (w/25ms sleep) and 99.9983% of time when
+   unpressed (w/100ms sleep).
 """
 
 import time
@@ -20,34 +25,35 @@ led = machine.Pin("LED", machine.Pin.OUT)
 def turn_led_on(): led.value(1)
 def turn_led_off(): led.value(0)
 
-def poll_bootsel_till_released(timeout=None, press_callback=None, release_callback=None):
-    pressed_duration = None
-    pressed_since = None
-    pressed_sleeptime = 25000
-    unpressed_sleeptime = 100000
-    if timeout:
-        timeout += time.time()
+def poll_bootsel_till_released(timeout_s=None, press_callback=None, release_callback=None):
+    pressed_duration_us = None
+    pressed_since_us = None
+    pressed_sleeptime_us = 25000
+    unpressed_sleeptime_us = 100000
+
+    if timeout_s:
+        timeout_s += time.time()
     
     while True:
         pressed = rp2.bootsel_button()
-
         if pressed:
-            if not pressed_since:
-                pressed_since = time.ticks_us()
+            if not pressed_since_us:
+                pressed_since_us = time.ticks_us()
                 if press_callback:
                     press_callback()
-            time.sleep_us(pressed_sleeptime)
+            time.sleep_us(pressed_sleeptime_us)
         else:
-            if not pressed_since:
-                if timeout and time.time() > timeout:
+            if not pressed_since_us:
+                if timeout_s and time.time() > timeout_s:
                     break
-                time.sleep_us(unpressed_sleeptime)
+                time.sleep_us(unpressed_sleeptime_us)
             else:
-                pressed_duration = time.ticks_us() - pressed_since
+                pressed_duration_us = time.ticks_us() - pressed_since_us
                 if release_callback:
                     release_callback()
                 break
-    return pressed_duration
+
+    return pressed_duration_us
 
 if __name__ == '__main__':
     
